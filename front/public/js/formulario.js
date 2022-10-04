b52a925daa0504ecd080c5991a328c4fb874884f
Yessenia <esseyros.27@gmail.com>
2022-10-04 00:38:28 -0500

    consumo servicios

--- front/public/js/formulario.js
+++ front/public/js/formulario.js
@@ -6,8 +6,14 @@ function enviarFormulario() {
     var nombreCliente = document.getElementById('nombreCliente').value;
     var telefono = document.getElementById('telefono').value;
     var tipoMascota = document.getElementById('tipoMascota').value;
-    
-    var data = {
+
+    var tipoServicio = document.getElementById('tipoServicio').value;
+
+    var fechaAgenda = document.getElementById('fechaAgenda').value;
+    var horaAgenda = document.getElementById('horaAgenda').value;
+    var fechaConHora = fechaAgenda + "T" + horaAgenda;
+
+    var dataCrearClienteMascota = {
         nombreMascota: nombreMascota,
         correo: correo,
         direccion: direccion,
@@ -19,10 +25,44 @@ function enviarFormulario() {
 
     fetch('http://localhost:8080/v1/clientemascota/crear-cliente-mascota', {
         method: "POST",
-        body: JSON.stringify(data),
+        body: JSON.stringify(dataCrearClienteMascota),
         headers: { "Content-type": "application/json; charset=UTF-8" }
     })
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err));
+        .then(responseClienteMascota => responseClienteMascota.json())
+        .then(jsonClienteMascota => {
+            //
+            var crearServicio = {
+                "idClienteMascota": jsonClienteMascota.idClienteMascota,
+                "tipoServicio": tipoServicio
+            };
+            fetch('http://localhost:8080/v1/clientemascota/crear-servicio', {
+                method: "POST",
+                body: JSON.stringify(crearServicio),
+                headers: { "Content-type": "application/json; charset=UTF-8" }
+            })
+                .then(responseServicio => responseServicio.json())
+                .then(jsonServicio => {
+                    ///
+                    var dataAgendarCita = {
+                        "idServicio": jsonServicio.idServicio,
+                        "idClienteMascota": jsonServicio.idClienteMascota,
+                        "fechaAgenda": fechaConHora
+                    };
+                    fetch('http://localhost:8080/v1/clientemascota/agendar-cita', {
+                        method: "POST",
+                        body: JSON.stringify(dataAgendarCita),
+                        headers: { "Content-type": "application/json; charset=UTF-8" }
+                    })
+                        .then(responseAgendarCita => responseAgendarCita.json())
+                        .then(jsonAgendarCita => {
+                            console.log(jsonAgendarCita)
+                        })
+                        .catch(err1 => console.log(err1));
+                    ///
+                    console.log(jsonServicio)
+                })
+                .catch(err2 => console.log(err2));
+            //
+        })
+        .catch(err3 => console.log(err3));
 }
